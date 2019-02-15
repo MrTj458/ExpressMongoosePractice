9f828b701310835f415b1c19ba1ceee390cc55ca
MrTj458 <mrtj458@gmail.com>
2019-02-15 01:46:42 -0700

    Added morgan

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const items = require('./routes/api/items')
 
 const app = express()
 
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
 
 mongoose
